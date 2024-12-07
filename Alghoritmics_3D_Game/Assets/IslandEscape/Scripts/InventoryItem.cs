cca928e133a842f253c73378ce5856c71a073f5f
Furkan Çakan <153424784+slenophlle@users.noreply.github.com>
2024-12-07 21:26:31 +0300

    .

--- Alghoritmics_3D_Game/Assets/IslandEscape/Scripts/InventoryItem.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class InventoryItem : MonoBehaviour
-{
-    public string itemName; // Ödül adı
-    public GameObject itemPrefab; // Oyun dünyasına yerleştirilecek model
-    public Sprite itemIcon; // Envanterde görünecek ikon
-}
